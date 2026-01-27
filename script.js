98d9e1d481f1fecd3d0e8fb9bbf2100e721551a5
Christian Ramos Sandoval <92647779+TheCrisKing10@users.noreply.github.com>
2026-01-26 18:28:34 -0600

    Update script.js

--- script.js
+++ script.js
@@ -7,12 +7,12 @@ let products = [
     // ======================
     { name: "Benson Crystal Violet 20's", price: 89.96, piecesPerPackage: 10, image: "(cigarros) benson crystal violet.webp", calcType: "multiply", unitLabel: "paquete", category: "Cigarros" },
 
-    { name: "Camel Yellow 20's", price: 78.00, piecesPerPackage: 10, image: "(cigarros) camel yellow.webp", calcType: "multiply", unitLabel: "paquete", category: "Cigarros" },
+    { name: "Camel Yellow 20's", price: 94.76, piecesPerPackage: 10, image: "(cigarros) camel yellow.webp", calcType: "multiply", unitLabel: "paquete", category: "Cigarros" },
 
-    { name: "Link Azul 20's", price: 34.00, piecesPerPackage: 10, image: "(cigarros) link azul.webp", calcType: "multiply", unitLabel: "paquete", category: "Cigarros" },
-    { name: "Link Negro 20's", price: 32.00, piecesPerPackage: 10, image: "(cigarros) link negro.webp", calcType: "multiply", unitLabel: "paquete", category: "Cigarros" },
-    { name: "Link Rojo 20's", price: 32.00, piecesPerPackage: 10, image: "(cigarros) link rojo.webp", calcType: "multiply", unitLabel: "paquete", category: "Cigarros" },
-    { name: "Link Rojo 100 20's", price: 38.00, piecesPerPackage: 10, image: "(cigarros) link rojo.webp", calcType: "multiply", unitLabel: "paquete", category: "Cigarros" },
+    { name: "Link Azul 20's", price: 44.00, piecesPerPackage: 10, image: "(cigarros) link azul.webp", calcType: "multiply", unitLabel: "paquete", category: "Cigarros" },
+    { name: "Link Negro 20's", price: 42.00, piecesPerPackage: 10, image: "(cigarros) link negro.webp", calcType: "multiply", unitLabel: "paquete", category: "Cigarros" },
+    { name: "Link Rojo 20's", price: 42.00, piecesPerPackage: 10, image: "(cigarros) link rojo.webp", calcType: "multiply", unitLabel: "paquete", category: "Cigarros" },
+    { name: "Link Rojo 100 20's", price: 42.00, piecesPerPackage: 10, image: "(cigarros) link rojo.webp", calcType: "multiply", unitLabel: "paquete", category: "Cigarros" },
     
     { name: "Marlboro Rojo 20's", price: 95.00, piecesPerPackage: 10, image: "(cigarros) marlboro rojo.webp", calcType: "multiply", unitLabel: "paquete", category: "Cigarros" },
     { name: "Marlboro Chester 25's", price: 82.08, piecesPerPackage: 8, image: "(cigarros) marlboro chesterfield.webp", calcType: "multiply", unitLabel: "paquete", category: "Cigarros" },
@@ -35,7 +35,7 @@ let products = [
     { name: "Shots 20's", price: 61.75, piecesPerPackage: 10, image: "(cigarros) shots 20.webp", calcType: "multiply", unitLabel: "paquete", category: "Cigarros" },
     { name: "Shots 25's", price: 69.70, piecesPerPackage: 8, image: "(cigarros) shots 25.webp", calcType: "multiply", unitLabel: "paquete", category: "Cigarros" },
     
-    { name: "Encendedor Tokai", price: 120.00, piecesPerPackage: 1, image: "(cigarros) tokai.webp", calcType: "multiply", unitLabel: "paquete", category: "Cigarros" },
+    { name: "Encendedor Tokai", price: 130.00, piecesPerPackage: 1, image: "(cigarros) tokai.webp", calcType: "multiply", unitLabel: "paquete", category: "Cigarros" },
 
     // ========================
     // 🥤 CATEGORÍA: COCA-COLA
